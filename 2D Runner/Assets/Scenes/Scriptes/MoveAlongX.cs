337183a147b540306a87ddc9e5b11602f8aa9da4
MrNiceGuy12312 <52978365+MrNiceGuy12312@users.noreply.github.com>
2019-10-15 16:10:22 -0400

    MainGame

---
+++ 2D Runner/Assets/Scenes/Scriptes/MoveAlongX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAlongX : MonoBehaviour
+{
+
+    public float moveSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x += moveSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+}
