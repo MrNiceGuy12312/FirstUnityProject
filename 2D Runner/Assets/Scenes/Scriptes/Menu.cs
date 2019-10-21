8b7f64a7fef325ef9c8888c3fbd0fc9b046c323e
iiMLeroyJenkz <56455387+iiMLeroyJenkz@users.noreply.github.com>
2019-10-21 16:49:06 -0400

    Added UI
    Main menu, character selection, htp, options screens

---
+++ 2D Runner/Assets/Scenes/Scriptes/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+
+{ 
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+    public void CharacterSelection ()
+{
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
+    public void Options ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void HowToPlay ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void Play ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
